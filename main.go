ad3b27e64b130ea18d10d5e08210d95adf7e72b9
anhilmy <hilmyahmadnaufal@gmail.com>
2023-08-07 14:02:30 +0700

    update dependancies

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	docs "sireg/rest-api-kegiatan/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/", index)
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/", index)
+	}
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	router.Run()
 }
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do piing
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} helloword
+// @Router / [get]
 func index(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "success",
